17aafe9c19979e09ab36b38171f1565d26bd9381
Manuel Serra <manuelserranunes@gmail.com>
2018-11-03 19:55:03 +0000

    main test other heuristics

--- mini_project1/main.py
+++ mini_project1/main.py
@@ -1,14 +1,32 @@
 from go import *
 
 
-def display (board):
+def display(board):
 
+    mystr = ""
     for row in board:
-        print(row)
-        print("\n")
+        mystr = ''.join([str(get_group_player(i)) for i in row])
+        print(mystr)
+        mystr = ""
+    print("\n")
 
-#file_path = "/Users/simaomoraessarmento/Dropbox/IST/S1/IA/Artificial_Intelligence_IST/mini_project1/examples/test_05.txt"
-file_path = "/Users/ManuelSerraNunes/Desktop/Tecnico_5_1S/IA/Artificial_Intelligence_IST/mini_project1/examples/test_05_long.txt"
+def get_group_player(group_number):
+    if group_number == 0:
+        return 0
+
+    if group_number % 2 == 0:
+        return 2
+    else:
+        return 1
+
+def convert_to_tuple(mystr):
+    a = (int(mystr[1]), int(mystr[3]), int(mystr[5]))
+    return a
+
+
+
+# file_path = "/Users/simaomoraessarmento/Dropbox/IST/S1/IA/Artificial_Intelligence_IST/mini_project1/examples/test_05.txt"
+file_path = "/Users/ManuelSerraNunes/Desktop/Tecnico_5_1S/IA/Artificial_Intelligence_IST/mini_project1/examples/test_aplhago.txt"
 file = open(file_path)
 
 
@@ -17,11 +35,21 @@ s = game.load_board(file)
 
 new_state = s
 
-while(new_state.)
-action = alphabeta_cutoff_search(new_state, game, d=4, cutoff_test=None, eval_fn=None)
-new_state = game.result(s, action)
+i = 1
 
-display(new_state.group_board)
+while not game.terminal_test(new_state):
+
+    if not(i % 2 == 0):
+        mystr = input("Next action: ")
+        action = convert_to_tuple(mystr)
+    else:
+        action = alphabeta_cutoff_search(new_state, game, d=4, cutoff_test=None, eval_fn=None)
+
+    new_state = game.result(new_state, action)
+
+    display(new_state.group_board)
+
+    i += 1
 
 
 
